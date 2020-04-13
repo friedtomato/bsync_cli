d29656c2b5097e409cd5d7aaecc459c59a0cefee
friedtomato <friedtomato@gmail.com>
2020-04-13 21:31:19 +0200

    fwsocket update

--- Fwsocket.pm
+++ Fwsocket.pm
@@ -6,9 +6,15 @@ my $IF_WLAN_NAME = "wlan0";
 my $IF_WLAN_IP = "un";
 
 sub get_wlan_ip {
-	my $if_wlan = IO::Interface::Simple->new($IF_WLAN_NAME);
-	$IF_WLAN_IP = $if_wlan->address;
-	undef $if_wlan;
+	my $end = 0;
+	while(!$end){
+		my $if_wlan = IO::Interface::Simple->new($IF_WLAN_NAME);
+		$IF_WLAN_IP = $if_wlan->address;
+		if(defined($IF_WLAN_IP)){
+			$end = 1;
+		}
+		undef $if_wlan;
+	}
 	print "wlan0 - ".$IF_WLAN_IP."\n";
 	return $IF_WLAN_IP;
 }
